35c30c259632cd98b97ff9aaa12e7ae3675fec58
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2023-04-30 16:29:45 -0300

    Configurações da Aplicação

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bp = require('body-parser')
+
+const app = express()
+
+// configurando o parser
+
+app.use(bp.json({ limit: '10mb' }))
+app.use(bp.urlencoded({ extended: false }))
+
+// configurando Front
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+//definindo arquivos estaticos
+app.use(express.static('public'))
+
+//chamando rotas
+app.use('/', (req, res)=>{
+    return res.send('Projeto esta rodando')
+}) 
+
+module.exports = app;
