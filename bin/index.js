46205f14ff291ecfa3ddf4b7a54cd277d4337a8e
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2023-05-01 00:41:38 -0300

    Configurando conexão com o banco

--- bin/index.js
+++ bin/index.js
@@ -8,16 +8,19 @@ const app = express()
 app.use(bp.json({ limit: '10mb' }))
 app.use(bp.urlencoded({ extended: false }))
 
+
 // configurando Front
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+
 //definindo arquivos estaticos
 app.use(express.static('public'))
 
+
 //chamando rotas
 app.use('/', (req, res)=>{
-    return res.send('Projeto esta rodando')
+    return res.render('login')
 }) 
 
 module.exports = app;
--- bin/keys.js
+++ bin/keys.js
@@ -3,7 +3,7 @@
 module.exports = {
     server: { port: process.env.port || 3000 },
     database: {
-        connection: process.env.connection || 'mongodb://127.0.0.1:27017'
+        connection: process.env.connection || 'mongodb+srv://mateusbezzan:17121996@cluster0.hukxrlv.mongodb.net/test'
     },
     auth: {
         secret: 'c1c2c3c4c5'
